7c6b12a10839d8812d3798780595f6baf46742a2
Satwato <stwtdey1@gmail.com>
2020-02-01 03:57:22 +0530

    titles and posts

--- reddit_scrape.py
+++ reddit_scrape.py
@@ -20,10 +20,49 @@ soup=BeautifulSoup(page.text, 'html.parser')
 
 #     print(domain.text)
 
-for domain in soup.find_all("span", class_="domain"):
-    if domain.text != "(self.SuicideWatch)":
-       #print(domain.text)
-        continue
+# for domain in soup.find_all("span", class_="domain"):
+#     if domain.text != "(self.SuicideWatch)":
+#        #print(domain.text)
+#         continue
     
-    parent_div = domain.parent
-    print(parent_div.text)
+#     parent_div = domain.parent
+#     print(parent_div.text)
+
+attrs = {'class': 'thing', 'data-domain': 'self.SuicideWatch'}
+
+
+    
+counter = 1
+words=[]
+while (counter <= 2):
+    for post in soup.find_all('div', attrs=attrs):
+        f= open("titles.txt","a+")
+        f2= open("posts.txt","a+")
+        link = post.find('a', class_="title").get('href')
+        #print(link)
+        title = post.find('a', class_="title").text
+        f.write("# "+title+"\n")
+        
+        p1 = requests.get("https://old.reddit.com"+link,headers=headers)
+        s1=BeautifulSoup(p1.text, 'html.parser')
+
+        if s1.find('div', attrs=attrs).find('div', class_="usertext-body") != None:
+            post=s1.find('div', attrs=attrs).find('div', class_="usertext-body").text
+            f2.write("# "+title+"\n")
+            f2.write("@ "+post+"\n")
+        else:
+            f2.write("# "+title+"\n")
+        words.extend(list(title.split(" ")))
+        
+        f.close()
+        f2.close()
+
+        #print(title)
+        #print(post)
+
+    next_button = soup.find("span", class_="next-button")
+    next_page_link = next_button.find("a").attrs['href']
+    time.sleep(2)
+    page = requests.get(next_page_link, headers=headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
+print(words)
