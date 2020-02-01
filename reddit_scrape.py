529d03b6dfde7f12cc103889b8fef602c78a2ce0
Satwato <stwtdey1@gmail.com>
2020-02-02 02:50:04 +0530

    trying to bypass only 1000 post barrier

--- reddit_scrape.py
+++ reddit_scrape.py
@@ -34,35 +34,45 @@ attrs = {'class': 'thing', 'data-domain': 'self.SuicideWatch'}
     
 counter = 1
 words=[]
-while (counter <= 2):
-    for post in soup.find_all('div', attrs=attrs):
-        f= open("titles.txt","a+")
-        f2= open("posts.txt","a+")
-        link = post.find('a', class_="title").get('href')
-        #print(link)
-        title = post.find('a', class_="title").text
-        f.write("# "+title+"\n")
-        
-        p1 = requests.get("https://old.reddit.com"+link,headers=headers)
-        s1=BeautifulSoup(p1.text, 'html.parser')
+try:
+  for i in tqdm(range(100)):
+      for post in soup.find_all('div', attrs=attrs):
+          f= open("titles.txt","a+")
+          f2= open("posts.txt","a+")
+          link = post.find('a', class_="title").get('href')
+          #print(link)
+          title = post.find('a', class_="title").text
+          f.write("# "+title+"\n")
+          
+          p1 = requests.get("https://old.reddit.com"+link,headers=headers)
+          s1=BeautifulSoup(p1.text, 'html.parser')
 
-        if s1.find('div', attrs=attrs).find('div', class_="usertext-body") != None:
-            post=s1.find('div', attrs=attrs).find('div', class_="usertext-body").text
-            f2.write("# "+title+"\n")
-            f2.write("@ "+post+"\n")
-        else:
-            f2.write("# "+title+"\n")
-        words.extend(list(title.split(" ")))
-        
-        f.close()
-        f2.close()
+          if s1.find('div', attrs=attrs).find('div', class_="usertext-body") != None:
+              post=s1.find('div', attrs=attrs).find('div', class_="usertext-body").text
+              f2.write("# "+title+"\n")
+              f2.write("@ "+post+"\n")
+          else:
+              f2.write("# "+title+"\n")
+          words.extend(list(title.split(" ")))
+          
+          f.close()
+          f2.close()
 
-        #print(title)
-        #print(post)
+          #print(title)
+          #print(post)
 
-    next_button = soup.find("span", class_="next-button")
-    next_page_link = next_button.find("a").attrs['href']
-    time.sleep(2)
-    page = requests.get(next_page_link, headers=headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
+      if soup.find("span", class_="next-button") != None:
+        next_button = soup.find("span", class_="next-button")
+        next_page_link = next_button.find("a").attrs['href']
+      else:
+        page = requests.get(next_page_link[:-16], headers=headers)
+        next_button = soup.find("span", class_="next-button")
+        next_page_link = next_button.find("a").attrs['href']
+      time.sleep(2)
+      page = requests.get(next_page_link, headers=headers)
+      soup = BeautifulSoup(page.text, 'html.parser')
+except AttributeError:
+  print(page.text)
+  print(soup)
+
 print(words)
